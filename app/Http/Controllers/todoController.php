d2e1d996909e033e67c8934aaa2dcb7c7610048d
indrapwa <gensbookcities@gmail.com>
2020-04-06 13:04:43 +0700

    install

---
+++ app/Http/Controllers/todoController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\ModelTodo;
+
+class ExampleController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+    public function index(){
+        $data = ModelTodo::all();
+        return response($data);
+    }public function show($id){
+        $data = ModelTodo::where('id',$id)->get();
+        return response ($data);
+    }public function store (Request $request){
+        $data = new ModelTodo();
+        $data->activity = $request->input('activity');
+        $data->description = $request->input('description');
+        $data->save();
+    
+        return response('Berhasil Tambah Data');
+    }
+}
